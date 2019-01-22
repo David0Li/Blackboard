78dc16f418d257439859b2fb612afa2b97a514fc
David <daiyun0li@gmail.com>
2019-01-22 17:53:51 -0500

    fesdf

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
 const http = require("http").Server(app);
